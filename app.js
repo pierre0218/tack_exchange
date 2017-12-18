faa6caf771a67991c5f5e50b7cc158d863c433d4
peterlee <peterlee@tizsoft.com>
2017-12-18 13:16:08 +0800

    first commit of tack exchange website

---
+++ app.js
@@ -0,0 +1,40 @@
+//module dependencies
+var express = require('express'); 
+var routes = require('./routes'); //it is equal to "./routes/index.js"
+var http = require('http');
+var path = require('path');
+var app = express();
+var partials = require('express-partials');
+// all environments
+
+//default port 3000
+app.set('port', process.env.PORT || 3000);
+
+
+app.set('views', path.join(__dirname, 'views'));//view tamplates
+app.set('view engine', 'ejs');
+app.use(partials());
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.bodyParser());
+app.use(express.cookieParser('123456789'));
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//routes
+app.get('/', routes.index);
+app.get('/t/:symbol', routes.index);
+app.get('/reg', routes.reg);
+app.post('/reg', routes.doReg);
+app.get('/login', routes.login);
+app.post('/login', routes.doLogin);
+app.get('/logout', routes.logout );
+
+http.createServer(app).listen(app.get('port'), function( req, res ){ 
+	//create app instance
+	console.log('Express server listening on port ' + app.get('port'));
+});
---
+++ routes/index.js
@@ -0,0 +1,85 @@
+﻿var tickersData = [
+	{ symbol: "BTC", lastusd: 18492, volume: 64486},
+	{ symbol: "LTC", lastusd: 306.71, volume: 439804},
+	{ symbol: "ETH", lastusd: 704.26, volume: 248080},
+	{ symbol: "XRP", lastusd: 0.69197, volume: 97147423},
+	{ symbol: "IOTA", lastusd: 3.834, volume: 34907679},
+]; 
+
+
+//Check user's login status
+var isLogin = false;
+var checkLoginStatus = function(req, res){
+	isLogin = false;
+	if(req.signedCookies.userid && req.signedCookies.password){
+		isLogin = true;
+	}
+};
+
+//Home page
+exports.index = function(req, res){
+	checkLoginStatus(req, res);
+	res.render( 'index', {
+		title : 'Tack Exchange', 
+		loginStatus : isLogin,
+		username : req.signedCookies.userid,
+		tickers: tickersData
+	});	
+};
+
+//Register page
+exports.reg = function(req, res){
+	checkLoginStatus(req, res);
+	res.render( 'reg', {
+		title : 'Register',
+		loginStatus : isLogin
+	});
+};
+
+//Do register action
+exports.doReg = function(req, res){
+	if(req.body['password-repeat'] != req.body['password']){
+		console.log('password not match');
+		console.log('first entered:' + req.body['password']);
+		console.log('second enterd:' + req.body['password-repeat']);
+		return res.redirect('/reg');
+	}
+	else{
+		//register success, redirect to index
+		res.cookie('userid', req.body['username'], { path: '/', signed: true});		
+		res.cookie('password', req.body['password'], { path: '/', signed: true });
+		return res.redirect('/');
+	}
+};
+
+//Login page
+exports.login = function(req, res){
+	checkLoginStatus(req, res);
+	res.render( 'login', {
+		title : 'Login',
+		loginStatus : isLogin
+	});
+};
+
+//Do login action
+exports.doLogin = function(req, res){
+	if(req.body['password-repeat'] != req.body['password']){
+		console.log('password not match');
+		console.log('first entered:' + req.body['password']);
+		console.log('second enterd:' + req.body['password-repeat']);
+		return res.redirect('/reg');
+	}
+	else{
+		//register success, redirect to index
+		res.cookie('userid', req.body['username'], { path: '/', signed: true});		
+		res.cookie('password', req.body['password'], { path: '/', signed: true });
+		return res.redirect('/');
+	}
+};
+
+//Do logout action
+exports.logout = function(req, res){
+	res.clearCookie('userid', { path: '/' });
+	res.clearCookie('password', { path: '/' });
+	return res.redirect('/');
+};
